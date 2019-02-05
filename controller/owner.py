6b85a0d5fb5a8acb3dc9c4b183cf6882845ea6b7
Siddharth Gangadhar <siddharth.gangadhar@gmail.com>
2019-02-05 15:59:43 -0500

    formatting changes

--- controller/owner.py
+++ controller/owner.py
@@ -18,12 +18,15 @@ def home():
 
 @owner_apis.route("/nearby-stores")
 def nearby_groceries():
-    params = {"key": Config.google_maps_api_key,
-              "location": Config.coords,
-              "rankby": "distance",
-              "keyword": "grocery"}
+    params = {
+        "key": Config.google_maps_api_key,
+        "location": Config.coords,
+        "rankby": "distance",
+        "keyword": "grocery",
+    }
     response = requests.get(
-        "https://maps.googleapis.com/maps/api/place/nearbysearch/json", params=params)
+        "https://maps.googleapis.com/maps/api/place/nearbysearch/json", params=params
+    )
     pprint(response.json(), stream=None, indent=2, width=80, depth=None)
     return jsonify(response.json())
 
@@ -31,10 +34,16 @@ def nearby_groceries():
 @owner_apis.route("/modify-price", methods=["POST"])
 def modify_price():
     params = request.json
-    return Response(response="Price adjusted", status=HTTP_STATUS_CODE.HTTP_200_OK, mimetype=None)
+    return Response(
+        response="Price adjusted", status=HTTP_STATUS_CODE.HTTP_200_OK, mimetype=None
+    )
 
 
 @owner_apis.route("modify-inventory", methods=["POST"])
 def add_inventory():
     params = request.json
-    return Response(response="Inventory added", status=HTTP_STATUS_CODE.HTTP_200_OK, mimetype="application/json")
+    return Response(
+        response="Inventory added",
+        status=HTTP_STATUS_CODE.HTTP_200_OK,
+        mimetype="application/json",
+    )

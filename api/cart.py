6dbdf58ae55fefd55a484de0096ad7ff083f1ef2
Siddharth Gangadhar <siddharth.gangadhar@gmail.com>
2024-12-25 17:46:53 -0500

    work on cart feature

---
+++ api/cart.py
@@ -0,0 +1,37 @@
+from models.entity.user_entity import User
+
+class Cart(object):
+    def __init__(self, user: User):
+        self.user = user
+        self.items = []
+
+    def add_item(self, item: dict):
+        self.items.append(item)
+
+    def remove_item(self, item: dict):
+        self.items.remove(item)
+
+    def get_cart(self):
+        return self.items
+    
+    def clear_cart(self):
+        self.items = []
+    
+    def get_total(self):
+        return sum(item['price'] for item in self.items)
+    
+    def checkout(self):
+        pass
+    
+    def get_cart_items(self):
+        return self.items
+    
+    def get_cart_total(self):
+        return self.get_total()
+
+    def get_cart_count(self):
+        return len(self.items)
+
+    def get_cart_items_total(self):
+        return sum(item['price'] for item in self.items)
+    

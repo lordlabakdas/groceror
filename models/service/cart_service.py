143653b18949e03542f8473e710798d137be6c6b
Siddharth Gangadhar <siddharth.gangadhar@gmail.com>
2025-02-22 17:26:38 -0500

    cart models and service layer

--- models/service/cart_service.py
+++ models/service/cart_service.py
@@ -1,6 +1,21 @@
+from models.entity.cart_entity import CartEntity
+from models.entity.cart_item_entity import CartItemEntity
+
 class CartService(object):
-    def __init__(self, cart_repository: CartRepository):
-        self.cart_repository = cart_repository
+    def __init__(self, cart_entity: CartEntity):
+        self.cart_entity = cart_entity
 
     def add_item(self, item: CartItemEntity):
-        self.cart_repository.add_item(item)
+        self.cart_entity.add_item(item)
+
+    def remove_item(self, item: CartItemEntity):
+        self.cart_entity.remove_item(item)
+
+    def clear(self):
+        self.cart_entity.clear()
+
+    def get_total_price(self):
+        return self.cart_entity.get_total_price()
+
+    def get_total_quantity(self):
+        return self.cart_entity.get_total_quantity()

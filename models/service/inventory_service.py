ee288427c6ac8a13a57f2548f61959ada4b39bbc
Siddharth Gangadhar <siddharth.gangadhar@gmail.com>
2025-03-02 19:19:06 -0500

    update ruff to latest version

---
+++ models/service/inventory_service.py
@@ -0,0 +1,27 @@
+from typing import List
+from models.entity.inventory_entity import Inventory
+from models.db import db_session
+
+
+class InventoryService:
+    def __init__(self, store_id: str):
+        self.store_id = store_id
+
+    def get_inventory(self) -> List[Inventory]:
+        return (
+            db_session.query(Inventory)
+            .filter(Inventory.store_id == self.store_id)
+            .all()
+        )
+
+    def add_inventory(
+        self, inventory: Inventory, quantity: int, price: float, store_id: str
+    ):
+        inventory.quantity = quantity
+        inventory.price = price
+        inventory.store_id = store_id
+        db_session.add(inventory)
+        db_session.commit()
+
+    def update_inventory(self, inventory: Inventory):
+        db_session.commit()
